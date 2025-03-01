e78d08ad5d2a3dc558fcfeb045b8946ef6c508de
BrandenJoe <147530777+BrandenJoe@users.noreply.github.com>
2025-02-28 18:05:45 -0600

    added a disable button when clicking on the other 2 buttons

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
 const timerMilliseconds = document.querySelector('.timer__milliseconds');
 const timerSeconds = document.querySelector('.timer__seconds');
 const timerMinutes = document.querySelector('.timer__minutes');
-
+const startButton = document.querySelector('.stopwatch__start');
+const stopButton = document.querySelector('.stopwatch__stop');
+const resetButton = document.querySelector('.stopwatch__reset');
 
 let startTime;
 let cancelId;
+let saveTime = 0;
 const countdown = 25 * 60 * 1000; // 25 minutes in milliseconds
 
 
 function startTimer(){
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = false;
 startTime = Date.now();
 cancelId = requestAnimationFrame(updateTimer);
 }
 function stopTimer(){
-
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = false;
+    saveTime += Date.now() - startTime ;
+cancelAnimationFrame(cancelId);
 }
 
 function resetTimer(){
-
+    startTime = Date.now();
+    saveTime = 0;
+    timerMilliseconds.innerHTML = "000"
+    timerSeconds.innerHTML = "00"
+    timerMinutes.innerHTML = "25"
 }
 
 function updateTimer(){
-    let millielapsed = Date.now() - startTime;
+    let millielapsed = Date.now() - startTime + saveTime;
+
+    let millisLeft = countdown - millielapsed;
+
+    if (millisLeft <= 0){
+        millisLeft = 0;
+        cancelAnimationFrame(cancelId);
+        cancelId = null;
+    }
 
     let timeleft = countdown - millielapsed;
     let secondleft = timeleft / 1000;
@@ -31,19 +53,22 @@ function updateTimer(){
     let secondsText = Math.floor(secondleft) % 60;
     let minutesText = Math.floor(minutesLeft);
 
-    if (minutesText.toString().length === 1){
+    if (minutesText.toString().length < 2){
         minutesText = minutesText.toString().padStart(2, '0');
     }
-    if (secondsText.toString().length === 1){
+    if (secondsText.toString().length < 2){
         secondsText = secondsText.toString().padStart(2, '0');
     }
-    if (milliText.toString().length === 1){
-        milliText = milliText.toString().padStart(2, '0');
+    if (milliText.toString().length < 3){
+        milliText = milliText.toString().padStart(3, '0');
     }
 
 
     timerMilliseconds.innerHTML = milliText
     timerSeconds.innerHTML = secondsText
     timerMinutes.innerHTML = minutesText
-    cancelId = requestAnimationFrame(updateTimer);
+
+    if (cancelId){
+        cancelId = requestAnimationFrame(updateTimer);
+    }
 }
